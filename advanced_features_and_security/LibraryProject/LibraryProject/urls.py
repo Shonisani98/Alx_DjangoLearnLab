88da787ffd79076d2be8685e690a3291df94208e
Shonisani Netsianda <sgheness@gmail.com>
2025-09-14 07:29:22 +0200

    Restructure project to match LibraryProject/LibraryProject/settings.py requirement

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
+    path('', include('LibraryProject.bookshelf.urls')),  # Adjust if your app is named differently
 ]
