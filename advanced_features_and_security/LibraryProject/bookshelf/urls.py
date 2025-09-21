618834e65b13746fc8a3b88c5bffc27b20789f2f
Shonisani Netsianda <sgheness@gmail.com>
2025-09-21 11:29:55 +0200

    Complete API setup with configuration, models, views, and routing

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++
@@ -1,7 +0,0 @@
-# LibraryProject/bookshelf/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Replace with your actual view
-]
--- api_project/settings.py
+++ api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dr9$9diilydiw_(e@siz+g!g!uja(gct(a1c$(ce_d1t)a54ov'
+SECRET_KEY = 'django-insecure-qdw@@u$$#ykw)-_irjlilev@5=xl63hrmyfpos2$+a90xoh(2d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
