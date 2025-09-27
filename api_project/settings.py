6e678c176934a99f9bb99e11770c061dc746d2f2
Shonisani Netsianda <sgheness@gmail.com>
2025-09-27 19:31:49 +0200

    Add rest_framework.authtoken to INSTALLED_APPS

--- api_project/settings.py
+++ api_project/settings.py
@@ -33,6 +33,10 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'api',
--- settings.py
+++ settings.py
@@ -33,10 +33,15 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'api',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
