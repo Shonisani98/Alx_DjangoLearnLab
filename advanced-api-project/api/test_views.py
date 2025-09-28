2130786a4dd2c0887f541572257a5cd0ebb6f5bf
Shonisani Netsianda <sgheness@gmail.com>
2025-09-28 18:33:00 +0200

    Switch to APITestCase for DRF test compliance

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,5 +1,10 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+
 
 class DummyTest(TestCase):
     def test_dummy(self):
         self.assertEqual(1, 1)
+class BookAPITestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        # rest of your setup...
