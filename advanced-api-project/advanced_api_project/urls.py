3a113ad7b2bd37ed7a677217366fcaa1117bca77
Shonisani Netsianda <sgheness@gmail.com>
2025-10-04 07:06:13 +0200

    Cleaned up project directory: removed legacy and unrelated folders

--- advanced-api-project/advanced_api_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # ✅ include is required
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),  # ✅ This line registers your app URLs
-]
--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings')
+>>>>>>> ae78151 (Initial Django blog setup: project, app, Post model, static/template dirs)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
