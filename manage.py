05d2cf7f6b51c6013c8c0a57804247f3c9a49bcf
Shonisani Netsianda <sgheness@gmail.com>
2025-10-12 18:01:40 +0200

    Fix manage.py syntax and indentation errors

--- manage.py
+++ manage.py
@@ -3,36 +3,17 @@
 import os
 import sys
 
-<<<<<<< HEAD
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
-=======
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings')
->>>>>>> 57d58c83f98c29ce6729a1cb84584b21e9889d5a
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-<<<<<<< HEAD
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
-=======
-            forget to activate a virtual environment?"
+            "available on your PYTHONPATH environment variable? Did you forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
->>>>>>> 57d58c83f98c29ce6729a1cb84584b21e9889d5a
