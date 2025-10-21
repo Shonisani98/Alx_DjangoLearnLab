20ebea9b183d5fc0f1271c08dd6210a486ace811
Shonisani Netsianda <sgheness@gmail.com>
2025-10-21 18:41:17 +0200

    Initial setup: recreated accounts app with CustomUser model and migrations

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
