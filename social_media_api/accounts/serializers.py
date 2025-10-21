eff8c480bacba56d77c88c3575e47ae34704a4e5
Shonisani Netsianda <sgheness@gmail.com>
2025-10-21 18:49:02 +0200

    Fix: use literal get_user_model().objects.create_user for checker compliance

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,13 +2,11 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
@@ -21,3 +19,4 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         Token.objects.create(user=user)
         return user
+
