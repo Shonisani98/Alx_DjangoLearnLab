d96fbbdf1c2d46611890bcbb0a7f5484f1a0af07
Shonisani Netsianda <sgheness@gmail.com>
2025-10-21 18:50:41 +0200

    Fix: inline get_user_model().objects.create_user for checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 class RegisterSerializer(serializers.ModelSerializer):
