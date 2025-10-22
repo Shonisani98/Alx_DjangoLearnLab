0336babde75c057d723a8b1c6c88adcefd2f0889
Shonisani Netsianda <sgheness@gmail.com>
2025-10-22 18:53:05 +0200

    Add checker-compliant follow/unfollow views and feed endpoint

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from django.urls import path
+from .views import FeedView
 
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
+urlpatterns += [
+    path('feed/', FeedView.as_view(), name='user-feed'),
 ]
