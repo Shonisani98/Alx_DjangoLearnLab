58b1ad42cdc880c5e70523d01df68741d430956d
Shonisani Netsianda <sgheness@gmail.com>
2025-10-22 18:50:56 +0200

    Add follow/unfollow views using GenericAPIView

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,20 +1,27 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import CustomUser
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def follow_user(request, user_id):
-    user_to_follow = get_object_or_404(CustomUser, id=user_id)
-    request.user.following.add(user_to_follow)
-    return Response({'detail': f'You are now following {user_to_follow.username}.'})
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def unfollow_user(request, user_id):
-    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
-    request.user.following.remove(user_to_unfollow)
-    return Response({'detail': f'You have unfollowed {user_to_unfollow.username}.'})
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        if user_to_follow == request.user:
+            return Response({'detail': 'You cannot follow yourself.'}, status=400)
+        if user_to_follow in request.user.following.all():
+            return Response({'detail': 'Already following this user.'}, status=400)
+        request.user.following.add(user_to_follow)
+        return Response({'detail': f'You are now following {user_to_follow.username}.'})
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        if user_to_unfollow not in request.user.following.all():
+            return Response({'detail': 'You are not following this user.'}, status=400)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'detail': f'You have unfollowed {user_to_unfollow.username}.'})
 
