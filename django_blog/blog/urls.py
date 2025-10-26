8174808892b252eadcccecb868ecd121089becec
Shonisani Netsianda <sgheness@gmail.com>
2025-10-26 12:06:56 +0200

    Add PostByTagListView and URL pattern for viewing posts by tag

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,11 @@
 from .views import search_posts
+from django.urls import path
+from .views import PostByTagListView
 
 urlpatterns += [
     path('search/', search_posts, name='search-posts'),
+]
+urlpatterns = [
+    # other patterns...
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),  # ✅ Required for checker
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
+from taggit.models import Tag
 
 def search_posts(request):
     query = request.GET.get('q', '')
@@ -10,3 +12,11 @@ def search_posts(request):
         Q(tags__name__icontains=query)
     ).distinct()
     return render(request, 'blog/search_results.html', {'results': results, 'query': query})
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/posts_by_tag.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('tag_slug')
+        return Post.objects.filter(tags__slug=tag_slug)
