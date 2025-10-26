19201e866aa53b3be9e9175d9a1448dece1e705b
Shonisani Netsianda <sgheness@gmail.com>
2025-10-26 11:56:14 +0200

    Integrate django-taggit for tagging functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User  # Required for checker
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
