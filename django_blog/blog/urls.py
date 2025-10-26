3cbe5ae01a121ec94c6e8e48085045010c954694
Shonisani Netsianda <sgheness@gmail.com>
2025-10-26 12:00:40 +0200

    Add PostByTagListView and URL pattern for viewing posts by tag

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,6 @@
-from .views import search_posts, posts_by_tag
+from django.urls import path
+from .views import PostByTagListView
 
 urlpatterns += [
-    path('search/', search_posts, name='search-posts'),
-    path('tags/<str:tag_name>/', posts_by_tag, name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
-def posts_by_tag(request, tag_name):
-    tag = get_object_or_404(Tag, name=tag_name)
-    posts = tag.posts.all()
-    return render(request, 'blog/posts_by_tag.html', {'tag': tag, 'posts': posts})
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,12 @@
-from django.db.models import Q
+from django.views.generic import ListView
+from taggit.models import Tag
+from .models import Post
 
-def search_posts(request):
-    query = request.GET.get('q', '')
-    results = Post.objects.filter(
-        Q(title__icontains=query) |
-        Q(content__icontains=query) |
-        Q(tags__name__icontains=query)
-    ).distinct()
-    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/posts_by_tag.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('tag_slug')
+        return Post.objects.filter(tags__slug=tag_slug)
