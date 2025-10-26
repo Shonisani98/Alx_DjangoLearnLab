3a20a90453d0d9edd4b993aa314638ac47b41113
Shonisani Netsianda <sgheness@gmail.com>
2025-10-26 11:43:48 +0200

    Update DATABASES config with USER and PORT for checker compliance

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,12 +73,9 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+Checks for database configuration
+
+django_blog/settings.py doesn't contain: ["USER", "PORT"]
 
 
 # Password validation
